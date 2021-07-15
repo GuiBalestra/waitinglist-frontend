1fc2e77936290ada2ce9462b24ec98b265fe883c
Guilherme Taveira <gb-ol@hotmail.com>
2021-07-15 14:42:29 -0300

    Navbar e  404

--- src/components/template/navbar/Navbar.vue
+++ src/components/template/navbar/Navbar.vue
@@ -1,15 +1,17 @@
 <template>
   <header class="header">
     <b-navbar toggleable="sm" type="dark" variant="info" class="nav">
-      <b-navbar-brand href="/">
-        <img src="../../../assets/images/logo.png" alt="abda_logo">
-      </b-navbar-brand>
+      <router-link :to="{ name: 'Dashboard' }">
+        <b-navbar-brand>
+          <img src="../../../assets/images/logo.png" alt="abda_logo">
+        </b-navbar-brand>
+      </router-link>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="fale-conosco">Links úteis</b-nav-item>
+          <router-link active-class="active" class="nav-link" :to="{ name: 'Links' }">Links</router-link>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
--- src/views/system/404.vue
+++ src/views/system/404.vue
@@ -1,15 +1,127 @@
 <template>
   <b-container>
-    <h1>404 - Página não encontrada</h1>
+    <PageTitle :title="title" />
+
+    <div class="d-flex justify-content-center">
+      <div class="monitor-external">
+        <div class="monitor-internal">404</div>
+      </div>
+    </div>
   </b-container>
 </template>
 
 <script>
-export default {
+import PageTitle from '@/components/pageTitle/PageTitle.vue'
 
+export default {
+  components: {
+    [PageTitle.name]: PageTitle
+  },
+
+  data: () => ({
+    title: 'Página não encontrada!'
+  })
 }
 </script>
 
 <style>
+  @media (min-width:  576px) {
+    .monitor-external {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 400px;
+      width: 500px;
+      border-radius: 10px;
+      background: var(--dark-low);
+      border: 5px solid var(--darker);
+    }
 
+    .monitor-internal {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 350px;
+      width: 450px;
+      border-radius: 5px;
+      background: var(--light-low);
+      border: 5px solid var(--darker);
+      font-size: 80px;
+    }
+  }
+
+  @media (max-width: 575px){
+    .monitor-external {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 300px;
+      width: 400px;
+      border-radius: 10px;
+      background: var(--dark-low);
+      border: 5px solid var(--darker);
+    }
+
+    .monitor-internal {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 250px;
+      width: 350px;
+      border-radius: 5px;
+      background: var(--light-low);
+      border: 5px solid var(--darker);
+      font-size: 80px;
+    }
+  }
+
+   @media (max-width: 500px){
+    .monitor-external {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 250px;
+      width: 350px;
+      border-radius: 10px;
+      background: var(--dark-low);
+      border: 5px solid var(--darker);
+    }
+
+    .monitor-internal {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 200px;
+      width: 300px;
+      border-radius: 5px;
+      background: var(--light-low);
+      border: 5px solid var(--darker);
+      font-size: 80px;
+    }
+  }
+
+   @media (max-width: 380px){
+    .monitor-external {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 200px;
+      width: 300px;
+      border-radius: 10px;
+      background: var(--dark-low);
+      border: 5px solid var(--darker);
+    }
+
+    .monitor-internal {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 150px;
+      width: 250px;
+      border-radius: 5px;
+      background: var(--light-low);
+      border: 5px solid var(--darker);
+      font-size: 80px;
+    }
+  }
 </style>
